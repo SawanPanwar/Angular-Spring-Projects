7110041b5921c10df988cb96a778358b00329733
SawanPanwar <skpanwar25@gmail.com>
2023-12-28 15:46:38 +0530

    ors10

---
+++ ORSProject10-UI/src/app/utility/data-validator.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class DataValidator {
+
+  constructor() {
+  }
+
+  isNotNullObject(val: any) {
+    let flag = true;
+    try {
+      if (val) {
+        flag = true;
+      } else {
+        flag = false;
+      }
+    } catch (error) {
+      flag = false;
+    }
+    return flag;
+  }
+}
